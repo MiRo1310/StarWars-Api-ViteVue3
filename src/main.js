07eb4b915ca86169590f03175546b10db1da6ec7
MiRo1310 <michael.roling@gmx.de>
2022-12-11 15:26:29 +0100

    Axios herausgenommen

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue'
 import './assets/tailwind.css'
 import App from './App.vue'
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
 import Pagination from 'v-pagination-3';
 
 
 const app = createApp(App)
-app.use(VueAxios, axios)
+// app.use(VueAxios, axios)
 app.component('pagination', Pagination);
 app.mount('#app')
